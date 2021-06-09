0bf2cd3e33f582ba3cdf367fe485f7277ab771cd
211925 <211925@1305-02>
2021-06-09 14:17:39 +0300

    Добавьте файлы проекта.

---
+++ KanyeWestQuotes/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanyeWestQuotes
+{
+    public class Root
+    {
+        public string quote { get; set; }
+    }
+}
---
+++ KanyeWestQuotes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace KanyeWestQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var ApiKey = "Your API Key";
+            //var City = "Kazan";
+            var url = $"https://api.kanye.rest";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+               // Console.WriteLine(result);
+                var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(weatherForecast.quote);
+            }
+
+        }
+    }
+}
