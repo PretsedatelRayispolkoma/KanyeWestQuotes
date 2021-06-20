3574a8b6803a55592ebbf44373a5681fda227dcd
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2021-06-20 19:35:12 +0300

    added new answers to my bot from kanye.restAPI

---
+++ Telebot/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    public class Root
+    {
+        public string quote { get; set; }
+    }
+}
---
+++ Telebot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelegramBotClient bot = new TelegramBotClient("1804492006:AAG0Yr3OnMk37L9qZVjVBTbVgrahPXPAO_I");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Your Yeezus quote is ''{GetQuote(arg.Message.Text)}''");
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+
+        static string GetQuote(string s )
+        {
+            var url = $"https://api.kanye.rest";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return "Error";
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var yeQuote = JsonConvert.DeserializeObject<Root>(result);
+                return $"{yeQuote.quote}";
+            }
+        }
+    }
+}
